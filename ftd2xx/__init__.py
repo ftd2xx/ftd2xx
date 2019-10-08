cb753f245b4d8b6f815794ee53862d2f001af211
wswongat <27878101+wswongat@users.noreply.github.com>
2019-10-08 11:23:22 +0800

    Update __init__.py
    For someone want to use the define constants

--- ftd2xx/__init__.py
+++ ftd2xx/__init__.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import
 import sys
 
 from .ftd2xx import *
-
+from .defines import *
 
 __all__ = ['call_ft', 'listDevices', 'getLibraryVersion', \
            'createDeviceInfoList', 'getDeviceInfoDetail', 'open', \
