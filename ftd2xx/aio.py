364cb4636c4dba15a10ead11c3acfff64cb87ffe
krister-ts <44512474+krister-ts@users.noreply.github.com>
2021-02-25 07:09:43 -0800

    Update aio.py
    Don't really need to use mixins

--- ftd2xx/aio.py
+++ ftd2xx/aio.py
@@ -1,8 +1,7 @@
-from .mixins import *
+from .ftd2xx import BaseFTD2XX
 
 class FTD2XX(BaseFTD2XX):
-  async def read(self, length: int) -> bytes:
-    while not self.getQueueStatus() >= length:
+  async def read(self, nchars: int, raw=True):
+    while not self.getQueueStatus() >= nchars:
       await asyncio.sleep(1e-3)  # Non-zero to save CPU (specific to my application, maybe there is a better general approach)
-    return self._read(length)
-  
+    return self._read(nchars, raw)
