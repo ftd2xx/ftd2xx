379acadac3c228dce64cd327db5b2d9bf37dedaa
Satya Mishra <satya.devel@gmail.com>
2021-03-03 17:58:41 -0600

    Fix flake8 errors and format with black

--- ftd2xx/aio/aio.py
+++ ftd2xx/aio/aio.py
@@ -1,25 +1,29 @@
 import asyncio
 import ctypes as c
-from ..defines import *
-from ..ftd2xx import *
-from ..ftd2xx import _ft
+from typing import Tuple
+from .. import defines
+from .. import ftd2xx
+from ..ftd2xx import _ft, call_ft
 
 
-class FTD2XX(FTD2XX):
+class FTD2XX(ftd2xx.FTD2XX):
     _timeouts = 0, 0
-    
+
     @property
-    def timeouts(self): return self._timeouts
-    
+    def timeouts(self):
+        return self._timeouts
+
     @timeouts.setter
-    def timeouts(self, timeouts: tuple):
+    def timeouts(self, timeouts: Tuple[float, float]):
         super().setTimeouts(*[1 if t > 0 else 0 for t in timeouts])
-        self._timeouts = tuple([t if t > 0 else 0 for t in timeouts])
-    
+        self._timeouts = [t if t > 0 else 0 for t in timeouts]
+
     async def read(self, nchars: int, raw=True):
         async def bytes_ready(n):
             while self.getQueueStatus() < nchars:
-                await asyncio.sleep(1e-3)  # Non-zero to save CPU (specific to my application, maybe there is a better general approach)
+                await asyncio.sleep(
+                    1e-3
+                )  # Non-zero to save CPU (specific to my application, maybe there is a better general approach)
 
         try:
             timeout, _ = self.timeouts
@@ -27,9 +31,10 @@ class FTD2XX(FTD2XX):
             await asyncio.wait_for(bytes_ready(nchars), timeout=timeout)
         finally:
             return super().read(nchars, raw)
-    
+
     def setTimeouts(self, read, write):
         self.timeouts = read, write
+
 
 def open(dev=0):
     """Open a handle to a usb device by index and return an FTD2XX instance for
@@ -38,7 +43,8 @@ def open(dev=0):
     call_ft(_ft.FT_Open, dev, c.byref(h))
     return FTD2XX(h)
 
-def openEx(id_str, flags=OPEN_BY_SERIAL_NUMBER):
+
+def openEx(id_str, flags=defines.OPEN_BY_SERIAL_NUMBER):
     """Open a handle to a usb device by serial number(default), description or
     location(Windows only) depending on value of flags and return an FTD2XX
     instance for it"""
