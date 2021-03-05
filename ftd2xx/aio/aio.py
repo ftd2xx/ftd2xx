9cb41d867b7de7afb7e3edcdfcc3e785ea3acaf1
Krister Wikstrom - RIMEX <krister.wikstrom@rimex.com>
2021-03-05 10:50:00 -0800

    Added `exc` kwarg to read and write.

--- ftd2xx/aio/aio.py
+++ ftd2xx/aio/aio.py
@@ -18,7 +18,7 @@ class FTD2XX(ftd2xx.FTD2XX):
         super().setTimeouts(*[1 if t > 0 else 0 for t in timeouts])
         self._timeouts = [t if t > 0 else 0 for t in timeouts]
 
-    async def read(self, nchars: int, raw=True):
+    async def read(self, nchars: int, raw=True, exc=False):
         async def bytes_ready(n):
             while self.getQueueStatus() < nchars:
                 await asyncio.sleep(
@@ -28,9 +28,21 @@ class FTD2XX(ftd2xx.FTD2XX):
         try:
             timeout, _ = self.timeouts
             timeout = (timeout - 1) / 1000.0 if timeout else None
+            if exc:
+                timeout += 1e-3
             await asyncio.wait_for(bytes_ready(nchars), timeout=timeout)
+        except asyncio.TimeoutError:
+            if exc:
+                raise
         finally:
             return super().read(nchars, raw)
+
+    def write(self, data, exc=False):
+        length = super().write(data)
+        if exc:
+            if length < len(data):
+                raise asyncio.TimeoutError
+        return length
 
     def setTimeouts(self, read, write):
         self.timeouts = read, write
