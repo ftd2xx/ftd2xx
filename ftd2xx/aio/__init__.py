61ac9e6e4d0ec9e6228a2190229250fe72dc5989
Krister Wikstrom - RIMEX <krister.wikstrom@rimex.com>
2022-02-25 00:10:25 +0000

    Add to module init

--- ftd2xx/aio/__init__.py
+++ ftd2xx/aio/__init__.py
@@ -3,4 +3,4 @@
 Module to use asyncio on FTD2XX object.
 """
 from ..ftd2xx import *
-from .aio import FTD2XX, open, openEx
+from .aio import FTD2XX, open, openEx, create_ftd2xx_connection, open_ftd2xx_connection
