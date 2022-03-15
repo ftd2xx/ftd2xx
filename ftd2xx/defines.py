8ad37dbb091fcee82c6fbe2a61d27017dfc22506
krister <krister.wikstrom@rimex.com>
2022-03-15 13:51:37 -0700

    Lots of changes to the ctypes - Cut out a lot of pthread and other stuff from linux - Moved common things like status and message codes to defines - Enumerated values in defines for typing

--- ftd2xx/defines.py
+++ ftd2xx/defines.py
@@ -1,3 +1,6 @@
+from enum import unique, IntFlag, IntEnum
+import sys
+
 # Statuses
 OK = 0
 INVALID_HANDLE = 1
@@ -24,22 +27,112 @@ LIST_NUMBER_ONLY = 0x80000000
 LIST_BY_INDEX = 0x40000000
 LIST_ALL = 0x20000000
 
-# OpenEx flags
-OPEN_BY_SERIAL_NUMBER = 1
-OPEN_BY_DESCRIPTION = 2
-OPEN_BY_LOCATION = 4
+
+@unique
+class OpenExFlags(IntFlag):
+    """Used to indicate the type of identifier being passed to FT_OpenEx."""
+
+    OPEN_BY_SERIAL_NUMBER = 1
+    OPEN_BY_DESCRIPTION = 2
+
+    if sys.platform == "win32":
+        OPEN_BY_LOCATION = 4
+
+
+OPEN_BY_SERIAL_NUMBER = OpenExFlags.OPEN_BY_SERIAL_NUMBER
+OPEN_BY_DESCRIPTION = OpenExFlags.OPEN_BY_DESCRIPTION
+if sys.platform == "win32":
+    OPEN_BY_LOCATION = OpenExFlags.OPEN_BY_LOCATION
+
+
+@unique
+class ModemStatus(IntFlag):
+    #: Clear to Send
+    CTS = 0x10
+
+    #: Data Set Ready
+    DSR = 0x20
+
+    #: Ring Indicator
+    RI = 0x40
+
+    #: Data Carrier Detect
+    DCD = 0x80
+
+    #: Data Ready
+    DR = 0x100
+
+    #: Overrun Error
+    OE = 0x200
+
+    #: Parity Error
+    PE = 0x400
+
+    #: Framing Error
+    FE = 0x800
+
+    #: Break Interrupt
+    BI = 0x1000
+
+    #: Transmitter Holding Register
+    THRE = 0x2000
+
+    #: Transmitter Empty
+    TEMT = 0x4000
+
+    #: Receiver FIFO Error
+    RCVE = 0x8000
+
+
+@unique
+class Device(IntEnum):
+    FT_232BM = 0
+    FT_232AM = 1
+    FT_100AX = 2
+    UNKNOWN = 3
+    FT_2232C = 4
+    FT_232R = 5
+    FT_2232H = 6
+    FT_4232H = 7
+    FT_232H = 8
+    FT_X_SERIES = 9
+
 
 # Device Identifiers
-DEVICE_232BM = 0
-DEVICE_232AM = 1
-DEVICE_100AX = 2
-DEVICE_UNKNOWN = 3
-DEVICE_2232C = 4
-DEVICE_232R = 5
-DEVICE_2232H = 6
-DEVICE_4232H = 7
-DEVICE_232H = 8
-DEVICE_X_SERIES = 9
+DEVICE_232BM = Device.FT_232BM
+DEVICE_232AM = Device.FT_232AM
+DEVICE_100AX = Device.FT_100AX
+DEVICE_UNKNOWN = Device.UNKNOWN
+DEVICE_2232C = Device.FT_2232C
+DEVICE_232R = Device.FT_232R
+DEVICE_2232H = Device.FT_2232H
+DEVICE_4232H = Device.FT_4232H
+DEVICE_232H = Device.FT_232H
+DEVICE_X_SERIES = Device.FT_X_SERIES
+
+
+@unique
+class Status(IntEnum):
+    OK = 0
+    INVALID_HANDLE = 1
+    DEVICE_NOT_FOUND = 2
+    DEVICE_NOT_OPENED = 3
+    IO_ERROR = 4
+    INSUFFICIENT_RESOURCES = 5
+    INVALID_PARAMETER = 6
+    INVALID_BAUD_RATE = 7
+    DEVICE_NOT_OPENED_FOR_ERASE = 8
+    DEVICE_NOT_OPENED_FOR_WRITE = 9
+    FAILED_TO_WRITE_DEVICE = 10
+    EEPROM_READ_FAILED = 11
+    EEPROM_WRITE_FAILED = 12
+    EEPROM_ERASE_FAILED = 13
+    EEPROM_NOT_PRESENT = 14
+    EEPROM_NOT_PROGRAMMED = 15
+    INVALID_ARGS = 16
+    NOT_SUPPORTED = 17
+    OTHER_ERROR = 18
+
 
 # Driver Types
 DRIVER_TYPE_D2XX = 0
