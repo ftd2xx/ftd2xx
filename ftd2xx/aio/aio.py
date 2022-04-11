a165f8ab7e1134363956f89043671f62ab9ff115
Satya Mishra <satya.devel@gmail.com>
2022-04-11 13:24:11 -0500

    Fix typings for Python < 3.8
    Also fix a few errors found by mypy

--- ftd2xx/aio/aio.py
+++ ftd2xx/aio/aio.py
@@ -48,18 +48,20 @@ class FTD2XX(ftd2xx.FTD2XX):
         self.timeouts = read, write
 
 
-def open(dev=0):
+def open(dev: int = 0, update: bool = True):
     """Open a handle to a usb device by index and return an FTD2XX instance for
     it"""
     h = _ft.FT_HANDLE()
     call_ft(_ft.FT_Open, dev, c.byref(h))
-    return FTD2XX(h)
+    return FTD2XX(h, update=update)
 
 
-def openEx(id_str, flags=defines.OPEN_BY_SERIAL_NUMBER):
+def openEx(
+    id_str: bytes, flags: int = defines.OPEN_BY_SERIAL_NUMBER, update: bool = True
+):
     """Open a handle to a usb device by serial number(default), description or
     location(Windows only) depending on value of flags and return an FTD2XX
     instance for it"""
     h = _ft.FT_HANDLE()
     call_ft(_ft.FT_OpenEx, id_str, _ft.DWORD(flags), c.byref(h))
-    return FTD2XX(h)
+    return FTD2XX(h, update=update)
